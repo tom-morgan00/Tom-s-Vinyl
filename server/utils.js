580f516b1d8fd14c00ea89f42b3d2476568cb8df
Tom Morgan <thomas.morgan1903@gmail.com>
2020-12-13 04:18:13 +0000

    another commit

--- server/utils.js
+++ server/utils.js
@@ -15,27 +15,9 @@ export const getToken = (user) => {
       name: user.name,
       email: user.email,
     },
-    process.env.JWT_SECRET,
+    process.env.JWT_SECRET || 'secret',
     {
       expiresIn: '24h',
     }
   );
-};
-
-export const isAuth = (req, res, next) => {
-  const token = req.headers.authorization;
-
-  if (token) {
-    const onlyToken = token.slice(7, token.length);
-    jwt.verify(onlyToken, process.env.JWT_SECRET, (err, decode) => {
-      if (err) {
-        return res.status(401).send({ message: 'Invalid Token' });
-      }
-      req.user = decode;
-      next();
-      return;
-    });
-  } else {
-    return res.status(401).send({ message: 'Token is not supplied.' });
-  }
 };
